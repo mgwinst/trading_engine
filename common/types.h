5e67e7db4657802aad2043e4d8c523fc841ebadf
mgwinst <matteospatola@hotmail.com>
2025-07-25 04:00:46 +0000

    some things to think about when using std::vector<std::pair<Price,Volume>>

---
+++ common/types.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <cstdint>
+
+namespace common {
+
+    enum class Side : uint8_t { bid, ask };
+
+    using Order_ID  = uint64_t;
+    using Ticker_ID = uint64_t;
+    using Price = uint64_t;
+    using Volume = uint64_t;
+    using Quantity = uint64_t;
+    using Priority = uint64_t;
+}

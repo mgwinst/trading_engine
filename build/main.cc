e4a2083e750a4144e704137f84c4d1afdf94a17e
mgwinst <matteospatola@hotmail.com>
2025-07-31 20:04:16 +0000

    for socket creation and socket options, move to strict asserts. Also using std::source_location for better debugging

--- build/main.cc
+++ build/main.cc
@@ -1,22 +1,10 @@
 #include <iostream>
+#include <fcntl.h>
+#include <cstring>
 
 #include "common/macros.hpp"
-#include "network/Socket.hpp"
+#include "network/socket.hpp"
 
 int main() {
-    
-    auto conf = network::SocketConfigFactory::from_interface("eno1", 1000, network::TransportLayerProtocol::TCP, true);
 
-    if (conf)
-        std::print("{}\n", conf.value().to_string());
-    else {
-        switch(conf.error()) {
-            case network::SocketConfigError::Interface_IP_Not_Found:
-                std::print(std::cerr, "Interface IP not found.\n");
-                break;
-            case network::SocketConfigError::Invalid_Port:
-                std::print(std::cerr, "Invalid port number.\n");
-        }
-    }
-    return 0;
 }
--- common/macros.hpp
+++ common/macros.hpp
@@ -1,14 +1,21 @@
 #pragma once
 
 #include <print>
+#include <source_location>
+#include <string_view>
 
-namespace common {
+namespace macros 
+{   
+    inline auto SOURCE_LOCATION(std::source_location location = std::source_location::current()) noexcept
+    {
+        return std::format("{}:{}:{}: {}", location.file_name(), location.line(), location.column(), location.function_name());
+    }
 
-    inline auto ASSERT(bool cond, const std::string_view msg) noexcept {
+    inline auto ASSERT(bool cond, const std::string_view message) noexcept 
+    {
         if (!cond) [[unlikely]] {
-            std::print("{}\n", msg);
+            std::println("{}", message);
             exit(EXIT_FAILURE);
         }
     }
-
 }

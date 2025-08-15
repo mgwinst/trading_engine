94d696e611630a5cfa51450e7671e8bfd489fdc9
mgwinst <matteospatola@hotmail.com>
2025-08-15 03:32:05 +0000

    xdp filter program

--- core/network/xdp_filter_mcast_udp.c
+++ core/network/xdp_filter_mcast_udp.c
@@ -10,7 +10,27 @@
 
 SEC("xdp_filter_mcast_udp")
 int xdp_filter(struct xdp_md* ctx) {
+    void* data = (void*)(long)ctx->data;
+    void* data_end = (void*)(long)ctx->data_end;
+    struct ethhdr* eth = data;
+    struct iphdr* ip; 
+    struct udphdr* udp;
+    
+    if (data + sizeof(*eth) + sizeof(*ip) + sizeof(*udp) > data_end)
+        return XDP_DROP;
 
+    if (eth->h_proto != __constant_htons(ETH_P_IP))
+        return XDP_DROP;
+
+    ip = data + sizeof(*eth);
+    if (ip->protocol != IPPROTO_UDP || ip->daddr != __constant_htonl(MCAST_IP_ADDR))
+        return XDP_DROP;
+
+    udp = (void*)ip + (ip->ihl << 2);
+    if (udp->dest != __constant_htons(UDP_PORT))
+        return XDP_DROP;
+
+    return XDP_PASS;
 }
 
 char _license[] SEC("license") = "GPL";
